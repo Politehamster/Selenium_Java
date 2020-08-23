9b65721599f72a201d1e7c7f5f93aa06414ecd78
Politehamster <42449275+Politehamster@users.noreply.github.com>
2020-08-23 10:36:51 +0600

    Semi-working prototype
    Switched to someone else's test for testing purpose

---
+++ README.md
@@ -0,0 +1,6 @@
+# To execute this docker-compose yml file use `docker-compose -f docker-compose.yml up`
+# Add the `-d` flag at the end for detached execution
+# To stop the execution, hit Ctrl+C, and then `docker-compose -f docker-compose.yml down`
+
+
+docker run -d -p 4444:4444 -p 5900:5900 -v /dev/shm:/dev/shm selenium/standalone-chrome-debug:3.141.59-yttrium
---
+++ selenium_tests/0004_Selenium_test_access_to_page.java
@@ -0,0 +1,20 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTestInSelenium {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+
+System.setProperty("webdriver.chrome.driver", ".\\Driver\\chromedriver.exe");
+WebDriver driver=new ChromeDriver();
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+driver.manage().window().maximize();
+driver.get("https://www.prospector.at");
+driver.close();
+
+}
+
+}
