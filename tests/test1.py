dbcc6325bff41b8c406d611f2342da8edd29eb91
Politehamster <cyberknight2438@gmail.com>
2020-09-01 20:24:58 +0600

    Fixed

--- tests/test1.py
+++ tests/test1.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-firefox = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX)
+firefox = webdriver.Remote(command_executor='http://selenium-hub:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX)
 
 firefox.get('https://www.google.com')
 print(firefox.title)
